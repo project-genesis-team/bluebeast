c9ca921c3df19b49624551cdac02988a8cccc789
Saint Crypto <jacobbrickane@gmail.com>
2019-02-22 07:53:18 +0100

    Add documentation for registry

--- SmartContracts/CMC.sol
+++ SmartContracts/CMC.sol
@@ -7,16 +7,25 @@ contract CMC is Ownable {
 
     mapping (bytes32 => address) public contracts;
 
+   /**
+    * @dev Adds contract to register
+    */
     function addContract(bytes32 _name, address _address) external onlyOwner {
         CMCEnabled cmcEnabled = CMCEnabled(_address);
         cmcEnabled.setCMCAddress(address(this));
         contracts[_name] = _address;
     }
 
+   /**
+    * @dev Get contract from register
+    */
     function getContract(bytes32 _name) external view returns (address) {
         return contracts[_name];
     }
 
+    /**
+    * @dev Remove contract from register
+    */
     function removeContract(bytes32 _name) external onlyOwner returns (bool) {
         require(contracts[_name] != 0x0);
         CMCEnabled cmcEnabled = CMCEnabled(contracts[_name]);
@@ -24,6 +33,9 @@ contract CMC is Ownable {
         contracts[_name] = 0x0;
     }
 
+    /**
+    * @dev Point contract to another registry
+    */
     function changeContractCMC(bytes32 _name, address _newCMC) external onlyOwner {
         CMCEnabled cmcEnabled = CMCEnabled(contracts[_name]);
         cmcEnabled.changeCMCAddress(_newCMC);
